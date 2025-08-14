583d71053e26f3df853aa25a9846a5ff4f3f9651
Max Richter <142268470+cleamax@users.noreply.github.com>
2025-08-14 09:05:56 +0200

    Create Dummy.t.sol

---
+++ challenges/Dummy.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+contract Dummy is Test {
+    function test_ok() public {
+        assertTrue(true);
+    }
+}
