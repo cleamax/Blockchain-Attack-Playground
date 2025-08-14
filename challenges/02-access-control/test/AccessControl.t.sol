cc2b8d6ab2772da5755f0c780c82affd9f744c84
Max Richter <142268470+cleamax@users.noreply.github.com>
2025-08-14 10:41:41 +0200

    Update AccessControl.t.sol

--- challenges/02-access-control/test/AccessControl.t.sol
+++ challenges/02-access-control/test/AccessControl.t.sol
@@ -9,28 +9,32 @@ contract AccessControlTest is Test {
     AdminVault vault;
     Attacker attacker;
     address user = address(0xBEEF);
+    address payable attackerEOA = payable(address(0xA11CE)); // Ziel für sweep()
 
     function setUp() public {
-        // deploy vault and fund it
+        // Vault deployen und initial funden
         vault = new AdminVault{value: 5 ether}();
 
-        // honest user also sends some ETH to the vault
+        // Honest user schickt zusätzlich 3 ETH
         vm.deal(user, 3 ether);
         vm.prank(user);
         (bool ok, ) = address(vault).call{value: 3 ether}("");
         require(ok, "funding fail");
 
-        // deploy attacker
+        // Attacker-Contract deployen; Owner = attackerEOA (empfängt spätere Auszahlung)
+        vm.prank(attackerEOA);
         attacker = new Attacker(vault);
     }
 
     function testExploitDrainsVault() public {
         uint256 beforeBal = address(vault).balance;
-        attacker.pwn();
+
+        attacker.pwn(); // grantAdmin -> sweep zum attackerEOA
+
         uint256 afterBal = address(vault).balance;
 
         assertEq(afterBal, 0, "vault should be empty");
-        assertGt(address(attacker.owner()).balance, 0, "attacker should receive funds");
+        assertGt(attackerEOA.balance, 0, "attacker EOA should receive funds");
         assertGt(beforeBal, afterBal, "balance must decrease");
     }
 }
